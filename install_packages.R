e51bb6847eb28d67845a57167cd43a0ac1e8fd5b
Alan Domínguez <81332368+alldominguez@users.noreply.github.com>
2024-05-31 16:17:37 +0200

    Add files via upload

--- install_packages.R
+++ install_packages.R
@@ -3,13 +3,17 @@
 # To install the packaged required for the practical session please follow the steps below:
 
 # install packages:
+install.packages("devtools")
+devtools::install_github("isglobal-brge/rexposome") # exposome package
+
+# Additionally we will instal the following packages
 list.of.packages <- c("Biobase", "mice", "MultiDataSet", "lsr", "FactoMiner",
                       "stingr", "circlize", "reshape2", "pryr", "scales", "imputeLCMD",
                       "scatterplot3d", "glmnet", "gridExtra", "grid", "Hmisc", "gplots",
                       "gtools", "S4Vectors", "tidyverse", "corrplot", "RColorBrewer",
-                      "skimr", "bkmr", "gWQS", "ggridges", "MASS", "caret", "partDSA")
+                      "skimr", "ggridges", "MASS", "caret", "partDSA", 
+                      "qWQS", "qgcomp", "bkmr") # packages for mixture analysis
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
 if (length(new.packages)) install.packages(new.packages)
 
-# Additionally we will instal the following packages
